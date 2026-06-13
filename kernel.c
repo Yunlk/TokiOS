3d2eb67064f94bae437d49f7108cba5f3f53b480
Yunlk <3283559503@qq.com>
2026-06-13 16:38:32 +0800

    init: multiboot kernel, VGA hello world

---
+++ kernel.c
@@ -0,0 +1,11 @@
+void kernel_main(void)
+{
+    char *video = (char*)0xb8000; // VGA text mode buffer
+    const char *msg = "TokiOS booted.";
+    for (int i = 0; msg[i]; i++) 
+    {
+        video[i * 2] = msg[i]; // Character
+        video[i * 2 + 1] = 0x0F;
+    }
+    while(1);
+}
