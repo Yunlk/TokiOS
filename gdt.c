ce3d230eb4de116cc087b0d6fff2a231ce1e358c
Yunlk <3283559503@qq.com>
2026-06-15 17:47:56 +0800

    fix: bug

--- gdt.c
+++ gdt.c
@@ -1,5 +1,5 @@
 #include "gdt.h"
-struct gdt_entry gdt[3];
+struct gdt_entry gdt[6];
 struct gdt_ptr gp;
 static tss_t tss;
 
@@ -19,7 +19,7 @@ static void gdt_set_gate(int num, uint32_t base, uint32_t limit, uint8_t access,
 
 void gdt_install()
 {
-    gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
+    gp.limit = (sizeof(struct gdt_entry) * 6) - 1;
     gp.base = (uint32_t)&gdt;
 
     gdt_set_gate(0, 0, 0, 0, 0);                                  // Null segment
